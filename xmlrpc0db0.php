8f8702a6bb9fe059020d18b1107d7e3bea8e88ed
fammouei1 <76709303+fammouei1@users.noreply.github.com>
2022-01-16 11:28:05 +0100

    Add files via upload

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://shoppart.store</homePageLink>
+		<homePageLink>https://makesaz.com</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://shoppart.store/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://shoppart.store/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://shoppart.store/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://shoppart.store/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://shoppart.store/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://makesaz.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://makesaz.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://makesaz.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://makesaz.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://makesaz.com/wp-json/" />
 			</apis>
 	</service>
 </rsd>
